58020e9536943931c7fc9f33d7cc27c6cba8490b
dongri <hdb151@naver.com>
2025-06-24 15:25:26 +0900

    20250624 myUtil

--- src/main/java/com/example/demo/MyUtil.java
+++ src/main/java/com/example/demo/MyUtil.java
@@ -1,5 +1,20 @@
 package com.example.demo;
 
 public class MyUtil {
+	
+	public static String getPwd()
+	{
+		String newPwd="";
+		
+		for(int i=1;i<5;i++)
+		{
+			int num=(int)(Math.random()*90);
+			
+			num=num+33;
+			
+			newPwd=newPwd+(char)num;
+		}
+		return newPwd;
+	}
 
 }

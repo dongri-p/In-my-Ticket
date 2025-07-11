65829b466f20528bf583c4fb44a055d20090c5ad
dongri <hdb151@naver.com>
2025-07-11 16:03:38 +0900

    20250711 reservation 초안

--- src/main/java/com/example/demo/reservation/ResService.java
+++ src/main/java/com/example/demo/reservation/ResService.java
@@ -1,5 +1,11 @@
 package com.example.demo.reservation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ResService {
 
+	@Autowired
+	private ResMapper mapper;
 }

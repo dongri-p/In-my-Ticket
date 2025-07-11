269b90306056601c686ad62640358283c2c202d5
dongri <hdb151@naver.com>
2025-07-11 16:28:14 +0900

    20250711 reserv

--- src/main/java/com/example/demo/reservation/ResController.java
+++ src/main/java/com/example/demo/reservation/ResController.java
@@ -2,10 +2,19 @@ package com.example.demo.reservation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ResController {
 
 	@Autowired
 	private ResService service;
+	
+	@GetMapping("/reservation/selectSeat")
+	public String selectSeat(HttpSession session)
+	{
+		return service.selectSeat(session);
+	}
 }
--- src/main/java/com/example/demo/reservation/ResService.java
+++ src/main/java/com/example/demo/reservation/ResService.java
@@ -3,9 +3,23 @@ package com.example.demo.reservation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.servlet.http.HttpSession;
+
 @Service
 public class ResService {
 
 	@Autowired
 	private ResMapper mapper;
+
+	public String selectSeat(HttpSession session)
+	{
+		if(session.getAttribute("userid") == null)
+		{
+			return "";
+		}
+		else
+		{
+			return "";
+		}	
+	}
 }

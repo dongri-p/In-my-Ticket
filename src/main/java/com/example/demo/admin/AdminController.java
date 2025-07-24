cade584c28cd9188b81d034e86f1fd47c6faa426
dongri <hdb151@naver.com>
2025-07-24 16:34:53 +0900

    20250724 admin

--- src/main/java/com/example/demo/admin/AdminController.java
+++ src/main/java/com/example/demo/admin/AdminController.java
@@ -1,12 +1,23 @@
 package com.example.demo.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class AdminController {
 	
-	@Qualifier
+	@Autowired
 	private AdminService service;
+	
+	@GetMapping("/admin/adLogin")
+	public String adLogin()
+	{
+		return service.adLogin();
+	}
+	
 
 }
--- src/main/java/com/example/demo/admin/AdminService.java
+++ src/main/java/com/example/demo/admin/AdminService.java
@@ -1,12 +1,18 @@
 package com.example.demo.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminService {
 	
-	@Qualifier
+	@Autowired
 	private AdminMapper mapper;
+	
+	public String adLogin()
+	{
+		return "/admin/adLogin";
+	}
 
 }

68288ee8b0ccf1d6edccc4d17ae48a04f9c2203e
dongri <hdb151@naver.com>
2025-07-25 21:08:54 +0900

    20250725 home

--- src/main/java/com/example/demo/admin/AdminService.java
+++ src/main/java/com/example/demo/admin/AdminService.java
@@ -1,7 +1,6 @@
 package com.example.demo.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
@@ -42,7 +41,7 @@ public class AdminService {
 	
 	public String home(HttpSession session, Model model)
 	{
-		return null;
+		return "/admin/home";
 	}
 
 }

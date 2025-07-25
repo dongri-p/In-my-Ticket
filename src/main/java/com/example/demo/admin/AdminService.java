847a20f4880538e794d613bdfc4ffe5cc5cb44a2
dongri <hdb151@naver.com>
2025-07-25 19:20:31 +0900

    20250725 adminlogin

--- src/main/java/com/example/demo/admin/AdminService.java
+++ src/main/java/com/example/demo/admin/AdminService.java
@@ -3,7 +3,9 @@ package com.example.demo.admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Service
@@ -12,8 +14,12 @@ public class AdminService {
 	@Autowired
 	private AdminMapper mapper;
 	
-	public String adLogin()
+	public String adLogin(HttpServletRequest request, Model model)
 	{
+		String err=request.getParameter("err");
+		
+		model.addAttribute("err", err);
+		
 		return "/admin/adLogin";
 	}
 

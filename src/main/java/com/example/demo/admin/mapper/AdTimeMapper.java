187b4c2a608a42844ea759c4ffaa3fc90853ba60
dongri <hdb151@naver.com>
2025-09-10 22:47:21 +0900

    20250910 adtimemapper

--- src/main/java/com/example/demo/admin/mapper/AdTimeMapper.java
+++ src/main/java/com/example/demo/admin/mapper/AdTimeMapper.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.demo.admin.dto.AdTimeDto;
+
 @Mapper
 public interface AdTimeMapper {
+	List<AdTimeDto> getTlist(int perfId);
 
 }
--- src/main/java/com/example/demo/admin/service/AdTimeService.java
+++ src/main/java/com/example/demo/admin/service/AdTimeService.java
@@ -1,9 +1,12 @@
 package com.example.demo.admin.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
+import com.example.demo.admin.dto.AdTimeDto;
 import com.example.demo.admin.mapper.AdTimeMapper;
 
 @Service
@@ -14,7 +17,7 @@ public class AdTimeService {
 
 	public String tmanage(int perfId, Model model)
 	{
-		
+		List<AdTimeDto> tlist=mapper.getTlist(perfId);
 		return "/admin/time/tmanage";
 	}
 

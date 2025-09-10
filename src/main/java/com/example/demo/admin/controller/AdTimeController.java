e19a5e0787686aedf1bb11021c2229777db273b8
dongri <hdb151@naver.com>
2025-09-10 22:21:29 +0900

    20250910 tmanage

--- src/main/java/com/example/demo/admin/controller/AdTimeController.java
+++ src/main/java/com/example/demo/admin/controller/AdTimeController.java
@@ -1,9 +1,15 @@
 package com.example.demo.admin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.example.demo.admin.service.AdTimeService;
 
 @Controller
 public class AdTimeController {
+	
+	@Autowired
+	private AdTimeService serive;
 	
 
 }
--- src/main/java/com/example/demo/admin/service/AdTimeService.java
+++ src/main/java/com/example/demo/admin/service/AdTimeService.java
@@ -3,6 +3,6 @@ package com.example.demo.admin.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TimeService {
+public class AdTimeService {
 
 }

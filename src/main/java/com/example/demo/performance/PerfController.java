ec3bd8d9e42b475a3ce3fe8d49bd499d43ca2653
dongri <hdb151@naver.com>
2025-09-23 15:09:51 +0900

    20250923 pDetail

--- src/main/java/com/example/demo/performance/PerfController.java
+++ src/main/java/com/example/demo/performance/PerfController.java
@@ -1,10 +1,13 @@
 package com.example.demo.performance;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -28,6 +31,28 @@ public class PerfController {
 	{
 		return pService.pDetail(perfId, model);
 	}
+	
+	@GetMapping("pathformance/getShowDate")
+	@ResponseBody
+	public List<String> getShowDate(@RequestParam int perfId)
+	{
+		return pService.getShowDate(perfId);
+	}
+	
+	@GetMapping("/performance/getShowTime")
+	@ResponseBody
+	public List<String> getShowTime(@RequestParam int perfId, @RequestParam String showDate)
+	{
+	    return pService.getShowTime(perfId, showDate);
+	}
+
+	@GetMapping("/performance/getRemainSeat")
+	@ResponseBody
+	public int getRemainSeat(@RequestParam int perfId, @RequestParam String showDate, @RequestParam String showTime)
+	{
+	    return pService.getRemainSeat(perfId, showDate, showTime);
+	}
+	
 	
 	
 

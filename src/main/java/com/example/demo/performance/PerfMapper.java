f7e2cb946533dc5dac67cbc28c06342506b0d1a9
dongri <hdb151@naver.com>
2025-09-23 15:05:41 +0900

    20250923 pDetail

--- src/main/java/com/example/demo/performance/PerfMapper.java
+++ src/main/java/com/example/demo/performance/PerfMapper.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface PerfMapper {
@@ -13,5 +14,7 @@ public interface PerfMapper {
 	public List<PerfDto> selectGenre(String genre);
 	public PerfDto pDetail(String perfId);
 	public String getTitle(int perfId);
-	public List<ShowDto> getShowList(int perfId);
+	public List<String> getShowDate(int perfId);
+	List<String> getShowTime(@Param("perfId") int perfId, @Param("showDate") String showDate);
+	int getRemainSeat(@Param("perfId") int perfId, @Param("showDate") String showDate,  @Param("showTime") String showTime);
 }
--- src/main/java/com/example/demo/performance/PerfService.java
+++ src/main/java/com/example/demo/performance/PerfService.java
@@ -37,16 +37,28 @@ public class PerfService {
 		
 		pdto.setRuntime(detailDto.getRuntime());
 		pdto.setGrade(detailDto.getGrade());
-		
-		List<ShowDto> showList=mapper.getShowList(perfId);
-		
+				
 		model.addAttribute("pdto", pdto);
-		model.addAttribute("showList", showList);
 		
 		 System.out.println("mt20id=" + mt20id + ", 관람시간=" + pdto.getRuntime() + ", 관람등급=" + pdto.getGrade());
 
 		
 		return "/performance/pDetail";	
+	}
+	
+	public List<String> getShowDate(int perfId)
+	{
+		return mapper.getShowDate(perfId);
+	}
+	
+	public List<String> getShowTime(int perfId, String showDate)
+	{
+		return mapper.getShowTime(perfId, showDate);
+	}
+	
+	public int getRemainSeat(int perfId, String showDate, String showTime)
+	{
+		return mapper.getRemainSeat(perfId, showDate, showTime);
 	}
 	
 

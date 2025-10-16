1723be36c199ab6fd8d1afb1f0a7a1d44bbe569b
dongri <hdb151@naver.com>
2025-10-16 14:39:09 +0900

    251016 rescontroller

--- src/main/java/com/example/demo/reservation/ResController.java
+++ src/main/java/com/example/demo/reservation/ResController.java
@@ -1,5 +1,7 @@
 package com.example.demo.reservation;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +21,17 @@ public class ResController {
 							 @RequestParam String time, Model model)
 	{
 		return service.selectSeat(perfId, date, time, model);
+	}
+	
+	@GetMapping("/reservation/payment")
+	public String payment(@RequestParam int perfId, @RequestParam String date,
+						  @RequestParam String time, @RequestParam List<String> seatIds, Model model)
+	{
+		model.addAttribute("perfId", perfId);
+		model.addAttribute("date", date);
+		model.addAttribute("time", time);
+		model.addAttribute("seatIds", seatIds);
+		return "/reservation/payment";
 	}
 	
 }
